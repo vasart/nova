4c74b2f0fd83afee5c5ea6f309d640ea7d1f84bb
anaumchev <anaumchev@gmail.com>
2014-07-21 16:57:28 -0400

    Revert "Merge remote-tracking branch 'origin/ssl_temporary_fix' into development"
    This reverts commit a9dcb14578d4941e7eaa225238b6add26390d6b3, reversing
    changes made to 4c7a842cf5e0a3d797ef8667aad0090c26f80008.

--- nova/scheduler/filters/trusted_filter.py
+++ nova/scheduler/filters/trusted_filter.py
@@ -110,7 +110,6 @@ class HTTPSClientAuthConnection(httplib.HTTPSConnection):
         sock = socket.create_connection((self.host, self.port), self.timeout)
         self.sock = ssl.wrap_socket(sock, self.key_file, self.cert_file,
                                     ca_certs=self.ca_file,
-                                    ssl_version=ssl.PROTOCOL_SSLv3,
                                     cert_reqs=ssl.CERT_REQUIRED)
 
 
